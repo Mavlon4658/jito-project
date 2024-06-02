373f0d679f644cdcfc3f23b606a34eb510135a90
Mavlon4658 <mavlonbe4658>
2024-06-02 10:56:39 +0500

    project create

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center bg-slate-400 text-white">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
